0a1076f06390d1ea636ddfde633bb1f1beffde95
Aaron Klinker <aaronklinker1@gmail.com>
2022-03-24 00:24:09 -0500

    chore: use sync package to manage spinner

--- cmd/log/spinner.go
+++ cmd/log/spinner.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/gosuri/uilive"
@@ -15,19 +16,38 @@ func Spinner(frames []string, status func() string) (func(error), func()) {
 	success := false
 	sleepDuration := 100 * time.Millisecond
 
+	var m sync.Mutex
+	isDone := func() bool {
+		m.Lock()
+		res := done
+		m.Unlock()
+		return res
+	}
+	isSuccess := func() bool {
+		m.Lock()
+		res := success
+		m.Unlock()
+		return res
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
 	stop := func(e error) {
+		m.Lock()
 		success = e == nil
 		done = true
-		time.Sleep(sleepDuration)
+		m.Unlock()
+		wg.Wait()
 	}
 	return stop, func() {
 		position := 0
-		for !done {
+		for !isDone() {
 			fmt.Fprintf(w, " %s %s\n", Loading(frames[position]), status())
 			time.Sleep(sleepDuration)
 			position = (position + 1) % frameCount
 		}
-		if success {
+		wg.Done()
+		if isSuccess() {
 			fmt.Fprintf(w, Success(" ✔ %s\n"), status())
 		} else {
 			fmt.Fprintf(w, Error(" ✘ %s\n"), status())
